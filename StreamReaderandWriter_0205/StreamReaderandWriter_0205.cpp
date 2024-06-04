a4b5836e9c152c3afca8942fdd9539024f6af7f9
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-06-04 14:52:37 +0700

    melakukan perulangan setiap baris

--- StreamReaderandWriter_0205/StreamReaderandWriter_0205.cpp
+++ StreamReaderandWriter_0205/StreamReaderandWriter_0205.cpp
@@ -36,6 +36,16 @@ int main() {
 	//jika file ada maka
 	if (infile.is_open())
 	{
-
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
 	}
+	//jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
