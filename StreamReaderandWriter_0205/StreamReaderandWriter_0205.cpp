b293bbb583c72703342801aa9e0902f37265986f
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-06-04 14:37:36 +0700

    membuka file dalam mode menulis

--- StreamReaderandWriter_0205/StreamReaderandWriter_0205.cpp
+++ StreamReaderandWriter_0205/StreamReaderandWriter_0205.cpp
@@ -4,3 +4,18 @@
 
 using namespace std;
 
+int main() {
+	string baris;
+
+	// membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\q untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+
+
+
+}
