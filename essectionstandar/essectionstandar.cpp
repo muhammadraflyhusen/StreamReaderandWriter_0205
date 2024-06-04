975d47365b6e6287fb28bb43d244c020ea67788a
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-06-04 15:05:31 +0700

    memasukan aray dan exeption di variabel

---
+++ essectionstandar/essectionstandar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exeption yang akan digunakan
+#include <array>
+//untuk obyek array yangakan kita gunakan
+using namespace std;
+int main()
+{
+	
+}
